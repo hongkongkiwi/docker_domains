fc3b55c3dd3c4d7f7074bca747066ded591dc40f
Andy <andy@savage.hk>
2017-06-11 22:15:30 +0800

    Fixed up some things

--- get_domains.sh
+++ get_domains.sh
@@ -5,7 +5,7 @@ TEMP_FILE="/tmp/domains.txt"
 REMOTE_SCRIPT="remote_docker_domains"
 REMOTE_HOST="root@192.168.1.3"
 REMOTE_PORT=22
-DEBUG="true"
+[[ "$1" == "-v" ]] && DEBUG="true" || DEBUG="false"
 
 command -v md5sum >/dev/null 2>&1 || { echo >&2 "I require md5sum but it's not installed.  Aborting."; exit 1; }
 command -v ssh >/dev/null 2>&1 || { echo >&2 "I require ssh but it's not installed.  Aborting."; exit 1; }
@@ -26,7 +26,7 @@ cat "$TEMP_FILE" > /dev/null
 GET_SUCCESS=$?
 
 [ "$GET_SUCCESS" == "0" ] || { echo >&2 "We could not access the remote domains.  Aborting."; exit 3; }
-[ `md5sum "$DEHYDRATED_DOMAINS" | cut -f1 -d " "` == `md5sum "$TEMP_FILE" | cut -f1 -d " "` ] && { echo >&2 "No new domains found.  Aborting."; exit 4; }
+[ `md5sum "$DEHYDRATED_DOMAINS" | cut -f1 -d " "` == `md5sum "$TEMP_FILE" | cut -f1 -d " "` ] && { [ "$DEBUG" == "true" ] && echo >&2 "No new domains found.  Aborting."; exit 4; }
 
 # Check whether we should update the domains.txt file
 if [ -f "$TEMP_FILE" -a \
